8cbaea25a0572c3f9ff04079ef0ecd8438343541
IngridyCandido <ingridyluzzia@gmail.com>
2026-06-24 20:04:49 -0300

    removendo node modules

--- api-inscricoes/dist/inscricoes/dto/create-inscricao.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CreateInscricaoDto {
-    nome: string;
-    email: string;
-    idade: number;
-    aceitaTermos: boolean;
-    interesses: string[];
-    observacoes?: string;
-}
